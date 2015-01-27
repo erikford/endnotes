293aed824d78e8d03c1c80ecacc6f061103f7e5d
Erik Ford <erikford@Eriks-MacBook-Pro.local>
2015-01-27 15:29:15 -0600

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# endnotes
+# Endnotes
 A simple solution for adding footnotes to your WordPress posts or pages.
+
+## Description
+Endnotes is a fork of John Watson’s [FD Footnotes](https://wordpress.org/plugins/fd-footnotes/) that has been updated to use the WordPress Settings API.
+
+## Installation
+1. Click the *Download Zip* button.
+2. In your WordPress dashboard, navigate to *Plugins -> Add New* and click the *Upload* tab.
+3. Upload the *zip archive* you downloaded.
+4. Once the download is complete, click *Activate Plugin*.
+
+## How to Use
+You can add footnotes to your posts or pages by adding numbered inline content within square brackets. Place the inline content where you would like the superscript numeral to appear.
+
+```
+I am Jack's happy sentence that will contain a footnote[1. I am Jack's happy footnote] for calling out a reference.
+```
+
+Each footnote must have a number followed by a period and a space and then the actual footnote. They don’t have to be unique but it is recommended. It doesn’t matter what the numbers are since the footnotes will be automatically renumbered when the post is displayed.
+
+Footnotes can contain anything you’d like including links, images, etc. Footnotes are automatically linked back to the spot in the text where the note was made.
+
+## Important Notes
+
+### Square Bracket Usage
+Do not include square brackets [] inside the footnotes themselves.
+
+### Unique Footnote Numbers
+Footnote numbers don't need to be unique but it is highly recommended, especially if the text is identical for multiple footnotes. If you have multiple footnotes with the exact same text and number then you’ll get undesirable results.
+
+### Replacing FD Footnotes with Endnotes
+If you would like to replace FD Footnotes with Endnotes, you will need to deactivate FD Footnotes *before* activating Endnotes. Because this plugin is properly using the WordPress Setting API, you will need to navigate to the *Endnotes Settings* page and update your settings.
+
+Once you have followed these steps, your footnotes should appear unaltered and function as expected.
---
+++ includes/endnotes-output.php
@@ -0,0 +1,74 @@
+<?php
+add_action( 'the_content', 'ef_endnotes_output', 1 );
+
+/**
+ * Endnotes Output
+ *
+ * Render endnotes.
+ *
+ * @package Endnotes
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Erik Ford <@okayerik>
+ *
+ */
+
+function ef_endnotes_output( $content ) {
+
+	$options    = get_options( '_ef_endnotes_settings' );
+	$header     = ( isset( $options['endnotes_header'] ) ) ? $options['endnotes_header'] : '';
+	$single     = ( isset( $options['endnotes_template'] ) ) ? $options['endnotes_template'] : '';
+	$collapse   = ( isset( $options['endnotes_collapse'] ) ) ? $options['endnotes_collapse'] : '';
+	$linksingle = false;
+
+	if ( !is_page() && !is_single() && $single ) $linksingle = true;
+
+	$post_id = get_the_ID();
+
+	$n = 1;
+	$notes = array();
+	if ( preg_match_all('/\[(\d+\. .*?)\]/s', $content, $matches ) ) {
+		foreach( $matches[0] as $fn ) {
+			$note = preg_replace( '/\[\d+\. (.*?)\]/s', '\1', $fn );
+			$notes[$n] = $note;
+
+			if ( $linksingle ) $singleurl = get_permalink();
+
+			$content = str_replace( $fn, "<sup class='footnote'><a href='$singleurl#fn-$post_id-$n' id='fnref-$post_id-$n' onclick='return fdfootnote_show($post_id)'>$n</a></sup>", $content );
+			$n++;
+		}
+
+		// *****************************************************************************************************
+		// Workaround for wpautop() bug. Otherwise it sometimes inserts an opening <p> but not the closing </p>.
+		// There are a bunch of open wpautop tickets. See 4298 and 7988 in particular.
+		$content .= "\n\n";
+		// *****************************************************************************************************
+
+		if ( !$linksingle ) {
+			$content .= "<div class='footnotes' id='footnotes-$post_id'>";
+
+			if ( $header ) {
+				$content .= "<h3>" . esc_html( $header ) . "</h3>";
+			}
+
+			if ( $collapse ) {
+				$content .= "<a href='#' onclick='return fdfootnote_togglevisible($post_id)' class='footnotetoggle'>";
+				$content .= "<span class='footnoteshow'>" . sprintf( _n( 'Show %d footnote', 'Show %d footnotes', $n - 1, 'endnotes' ), $n - 1 ) . "</span>";
+				$content .= "</a>";
+				
+				$content .= "<ol style='display: none'>";
+			} else {
+				$content .= "<ol>";
+			}
+			for( $i = 1; $i < $n; $i++ ) {
+				$content .= "<li id='fn-$post_id-$i'>$notes[$i] <span class='footnotereverse'><a href='#fnref-$post_id-$i'>&#8617;</a></span></li>";
+			}
+			$content .= "</ol>";
+			$content .= "</div>";
+
+		}
+
+	}
+
+	return( $content );
+}
